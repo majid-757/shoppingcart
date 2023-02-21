7fbf4f907228d8b9ed04c9b75670eab77c23e832
unknown <majid.asadollahi95@gmail.com>
2023-02-21 19:28:03 +0330

    create function buyCoure and getCourseInfo and eventLisener

--- js/app.js
+++ js/app.js
@@ -1,8 +1,41 @@
 // variables
-
+const courses = document.querySelector("#courses-list");
 
 // eventListeners
-
+eventListeners()
+function eventListeners() {
+    courses.addEventListener('click', buyCourse)
+}
 
 // functions
+
+// add the courses to the cart
+function buyCourse(e) {
+    e.preventDefault()
+    // use delegation for access to the course that selected
+    if (e.target.classList.contains('add-to-cart')) {
+        // access to the card div with parentElement
+        const course = e.target.parentElement.parentElement
+
+        // read value
+        getCoursInfo(course)
+
+    }
+    
+}
+
+// getting the course info that selected by user
+function getCoursInfo(course) {
+    const courseInfo = {
+        image: course.querySelector('img').src,
+        title: course.querySelector('h4').textContent,
+    }
+
+    console.log(courseInfo)
+}
+
+
+
+
+
 
