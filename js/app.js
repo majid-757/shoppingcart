b70e4c8f4e7f88b84f13ce3278920f5784ccb52f
unknown <majid.asadollahi95@gmail.com>
2023-02-22 16:27:59 +0330

    create function add to cart

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 // variables
-const courses = document.querySelector("#courses-list");
+const courses = document.querySelector("#courses-list"),
+    shoppingCartContent = document.querySelector('#cart-content tbody');
 
 // eventListeners
 eventListeners()
 function eventListeners() {
     courses.addEventListener('click', buyCourse)
+
+    // remove course from cart
+    shoppingCartContent.addEventListener('click', removeCourse)
+
 }
 
 // functions
@@ -26,16 +31,53 @@ function buyCourse(e) {
 
 // getting the course info that selected by user
 function getCoursInfo(course) {
+
+    // course info
     const courseInfo = {
         image: course.querySelector('img').src,
         title: course.querySelector('h4').textContent,
+        price: course.querySelector('span').textContent,
+        id: course.querySelectorAll('a')[1].getAttribute('data-id'),
     }
 
-    console.log(courseInfo)
+    // adding the course to the cart
+    addToCart(courseInfo)
 }
 
 
 
+// adding the course to the cart
+function addToCart(courseInfo) {
+    // create <tr> tag
+    let row = document.createElement('tr') 
 
+    // build HTML Template 
+    row.innerHTML = `
+        <tr>
+            <td>
+                <img src="${courseInfo.image}" width = '100px'>
+            </td>
+            <td>${courseInfo.title}</td>
+            <td>${courseInfo.price}</td>
+            <td>
+                <a class="remove" href="#" data-id="${courseInfo.id}">X</a>
+            </td>
+        </tr>
+
+    
+    `
+    shoppingCartContent.appendChild(row)
+
+    
+}
+
+
+
+// remove course
+function removeCourse(e) {
+    if (e.target.classList.contains('remove')) {
+        e.target.parentElement.parentElement.remove()
+    }
+}
 
 
